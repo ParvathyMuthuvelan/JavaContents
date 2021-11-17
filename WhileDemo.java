a5e5d357cf8e9c06b55d8395aea0b7ef1d5a7226
ParvathyMuthuvelan <78962020+ParvathyMuthuvelan@users.noreply.github.com>
2021-11-17 15:34:48 +0530

    Add files via upload

---
+++ WhileDemo.java
@@ -0,0 +1,21 @@
+
+public class WhileDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5;
+		int i=1;
+		while(i<=n)
+		{
+			System.out.println("while loop="+i);
+			i++;
+		}
+		do
+		{
+			System.out.println("do while loop="+i);
+			i++;
+		}while(i<=n);
+
+	}
+
+}
