fa97effe9adc4211022aa00eb1ac4789c945acfb
ParvathyMuthuvelan <78962020+ParvathyMuthuvelan@users.noreply.github.com>
2021-11-18 12:28:05 +0530

    Add files via upload

--- ArrayOfObjectsDemo.java
+++ ArrayOfObjectsDemo.java
@@ -3,41 +3,39 @@ import java.util.Scanner;
 public class ArrayOfObjectsDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scanner =new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 		long eid;
-		String name,job;
+		String name, job;
 		double sal;
-		int i,n;
-		//int arr[]=new int[10];
-		//Employee emp[] =new Employee[5];
+		int i, n;
+		// int arr[]=new int[10];
+		// Employee emp[] =new Employee[5];
+	//Employee emp1[]= {new Employee(1,"A",30000,"Manager"),new Employee()};
 		Employee emp[];
 		System.out.println("enter the number of employees");
-		n=scanner.nextInt();
-		emp=new Employee[n];//allocating memory for array
-		//Employee emp1=new Employee();
-		for(i=0;i<n;i++)
-		{
-		System.out.println("Etner id,name,job and salary");
-		eid=scanner.nextLong();
-		scanner.nextLine();
-		name=scanner.nextLine();
-		job=scanner.nextLine();
-		sal=scanner.nextDouble();
-		emp[i]=new Employee();//allocating memory for individual object
-		emp[i].getData(eid, name, sal, job);
-		
+		n = scanner.nextInt();
+		emp = new Employee[n];// allocating memory for array
+		// Employee emp1=new Employee();
+		for (i = 0; i < n; i++) {
+			System.out.println("Etner id,name,job and salary");
+			eid = scanner.nextLong();
+			scanner.nextLine();
+			name = scanner.nextLine();
+			job = scanner.nextLine();
+			sal = scanner.nextDouble();
+			emp[i] = new Employee();// allocating memory for individual object
+			emp[i].getData(eid, name, sal, job);
+
 		}
 		System.out.println("Employee Details");
-		for(i=0;i<n;i++)
-		{
+		for (i = 0; i < n; i++) {
 			emp[i].putData();
 		}
 		System.out.println("Traversing object array with for-each loop");
-		for(Employee obj:emp)
-		{
+		for (Employee obj : emp) {
 			obj.putData();
 		}
+		
 
 	}
 
