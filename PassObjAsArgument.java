6a8242de8813fde871a118af1cda52d4179e3969
ParvathyMuthuvelan <78962020+ParvathyMuthuvelan@users.noreply.github.com>
2021-11-18 15:19:11 +0530

    Add files via upload

--- PassObjAsArgument.java
+++ PassObjAsArgument.java
@@ -22,7 +22,7 @@ class Test
 		temp.b=temp.b*2;
 		System.out.println("In modify method a="+temp.a+" b="+temp.b);
 	}
-	//temp=t1
+	//temp=t1(5,10)
 	Test update(Test temp)
 	{
 		Test result=new Test();
@@ -38,15 +38,15 @@ public class PassObjAsArgument {
 	Test t1=new Test();
 	Test t2=new Test();
 	t2.input(3, 4);
-	System.out.println("Before calling method a="+t2.a+" b="+t2.b);//3,4
+	System.out.println("Before calling method a="+t2.a+" b="+t2.b);
 	t2.change(t2.a, t2.b);//8,8
 	//t2.modify(t2);//8,8
-	System.out.println("After calling method a="+t2.a+" b="+t2.b);//3,4
-    t1.input(5,10);
+	System.out.println("After calling method a="+t2.a+" b="+t2.b);
+ t1.input(5,10);
     //r=result
-    Test r=t1.update(t1);
+ Test r=t1.update(t1);
     
-    System.out.println("After calling update method a="+r.a+" b="+r.b);//6,11
+   System.out.println("After calling update method a="+r.a+" b="+r.b); 
 	}
 
 }
